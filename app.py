ef5b30f33c71b95225249738586e125133f3838e
4nemi <4nemi.aaa@gmail.com>
2024-02-13 11:46:42 +0900

    add 1997

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import streamlit as st
 import pandas as pd
+import os
 
-from utils.utils import set_custom_style, display_item
+from utils.utils import display_item
 
 
 @st.cache_data
-def load_data(participant_id: int):
-    df = pd.read_csv(f"data/10000/proposed_{participant_id}.csv")
+def load_data(file_path):
+    df = pd.read_csv(file_path)
     df = df[["anime_id", "name", "genres", "type", "episodes", "aired", "image_url"]]
     return df
+
+
+def download_evaluations(evaluations: dict):
+    df = pd.DataFrame.from_dict(evaluations, orient="index").reset_index()
+    df.columns = ["file", "anime_id", "interest", "unknown", "discovery"]
+    st.download_button(label="ダウンロード", data=df.to_csv(index=False), file_name="evaluations.csv", mime="text/csv")
 
 
 def main():
@@ -17,26 +24,51 @@ def main():
         page_icon=":tv:",
         layout="wide",
     )
-    participant_id = st.text_input("参加者IDを入力してください", '')
+    csv_files = [
+        "explicit.csv",
+        "unknown.csv",
+        "proposed.csv",
+        "random.csv",
+    ]
+
+    participant_id = st.text_input("参加者IDを入力してください")
     if participant_id:
-        df = load_data(participant_id)
-        df = df.sample(8)
-        items = df.to_dict(orient="records")
+        dir_path = f"data/{participant_id}"
+        if os.path.exists(dir_path):
+            files = [os.path.join(dir_path, file) for file in csv_files]
 
-        if "displayed_anime" not in st.session_state:
-            st.session_state.displayed_anime = []
-        if "interest" not in st.session_state:
-            st.session_state.interest = {}
-        if "unknown" not in st.session_state:
-            st.session_state.unknown = {}
+            if "current_file_index" not in st.session_state:
+                st.session_state.current_file_index = 0
+            if "evaluations" not in st.session_state:
+                st.session_state.evaluations = {}
 
-        for i in range(0, len(items), 4):
-            cols = st.columns(3)
-            for col, item in zip(cols, items[i : i + 4]):
-                with col:
-                    interest, unknown = display_item(item)
-                    st.session_state.interest[item["anime_id"]] = interest
-                    st.session_state.unknown[item["anime_id"]] = unknown
+            # stepを表示
+            st.write(f"Step: {st.session_state.current_file_index + 1}")
+
+            if st.session_state.current_file_index < 4:
+                file_path = files[st.session_state.current_file_index]
+                df = load_data(file_path)
+                items = df.to_dict(orient="records")
+                for i in range(0, len(items), 3):
+                    cols = st.columns(3)
+                    for col, item in zip(cols, items[i : i + 3]):
+                        with col:
+                            interest, unknown, discovery = display_item(item, st.session_state.current_file_index)
+                            st.session_state.evaluations[(os.path.basename(file_path), item["anime_id"])] = {
+                                "interest": interest,
+                                "unknown": unknown,
+                                "discovery": discovery,
+                            }
+
+                if st.button("次へ"):
+                    st.session_state.current_file_index += 1
+                    st.rerun()
+            else:
+                st.write("すべての評価が完了しました！ありがとうございました！")
+                st.write("以下のボタンをクリックして評価をダウンロードしてください")
+                download_evaluations(st.session_state.evaluations)
+        else:
+            st.write("参加者IDが見つかりませんでした")
 
 
 if __name__ == "__main__":
--- utils/utils.py
+++ utils/utils.py
@@ -1,25 +1,7 @@
 import streamlit as st
 
 
-def set_custom_style():
-    custom_style = """
-    <style>
-        /* ページ全体のマージンとパディングを変更 */
-        .css-18e3th9 {
-            padding: 0px;  /* 例：上下左右のパディングを20pxに設定 */
-            margin: 0px;   /* 例：上下左右のマージンを10pxに設定 */
-        }
-        /* アイテム間のマージンとパディングを変更 */
-        .stImage, .stSlider {
-            margin: 0px 0;  /* 例：上下のマージンを15pxに設定 */
-            padding: 5px;    /* 例：パディングを5pxに設定 */
-        }
-    </style>
-    """
-    st.markdown(custom_style, unsafe_allow_html=True)
-
-
-def display_item(item):
+def display_item(item, step):
     st.image(item["image_url"], width=200)
     st.write(item["name"])
     with st.expander("Show more"):
@@ -29,27 +11,7 @@ def display_item(item):
         st.write(f"放送日: {item['aired']}")
 
     # スライダーを使って評価を入力
-    interest = st.slider("興味", 1, 5, 3, key=f"{item['anime_id']}_interest")
-    unknown = st.slider("知らなさ", 1, 5, 3, key=f"{item['anime_id']}_unknown")
-    return interest, unknown
-
-
-"""
-def display_item(col, item):
-    col.markdown(
-        f"<div style='text-align' center'><img src='{item['image_url']}' style='max-width: 100%; height: auto;'></div>",
-        unsafe_allow_html=True,
-    )
-
-    with col.expander("Show more"):
-        col.write(f"ジャンル: {item['genres']}")
-        col.write(f"タイプ: {item['type']}")
-        col.write(f"エピソード数: {item['episodes']}")
-        col.write(f"放送日: {item['aired']}")
-
-    # スライダーを使って評価を入力
-    interest = col.slider("興味", 1, 5, 3, key=f"{item['anime_id']}_interest")
-    unknown = col.slider("知らなさ", 1, 5, 3, key=f"{item['anime_id']}_unknown")
-
-    return interest, unknown
-"""
+    interest = st.slider("興味", 1, 5, 3, key=f"{item['anime_id']}_{step}_interest")
+    unknown = st.slider("知らなさ", 1, 5, 3, key=f"{item['anime_id']}_{step}_unknown")
+    discovery = st.slider("発見性", 1, 5, 3, key=f"{item['anime_id']}_{step}_discovery")
+    return interest, unknown, discovery
